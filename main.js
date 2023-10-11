87600d12c3f2c45b2a986c9d3c4e0e99e3303fe9
Mina <msanford24@basecampcodingacademy.org>
2023-10-11 13:53:15 -0500

    link1

--- main.js
+++ main.js
@@ -1,21 +1,48 @@
-const doc = document.getElementsByClassName('preview-link')
+const previewLinks = document.getElementsByClassName('preview-link')
+// const doc = document.getElementsByClassName('preview-link').addEventListener("mouseover", hoverMouse)
+for (const link of previewLinks) {
+    link.addEventListener("mouseover", hoverMouse);
+    link.addEventListener("mouseout", hidePreview); 
+  }
 
+  const previewDiv = document.createElement('div');
+  previewDiv.classList.add('preview-container');
 
-
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-var requestOptions = {
-    method: "get",
-    headers: myHeaders,
-    redirect: "follow",
+  document.body.appendChild(previewDiv);
+  
+  function hoverMouse(event) {
+    const link = event.currentTarget;
     
-};
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow"
+    };
+  
+    fetch("https://v1.nocodeapi.com/minalouise/link_preview/JArWrIzvAjngbMhY?url=" + link.href, requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        const data = JSON.parse(result);
+        const imageUrl = data.image;
 
-fetch("https://v1.nocodeapi.com/minalouise/link_preview/JArWrIzvAjngbMhY?url=https://minalouise.github.io/GOAT-project/", requestOptions)
-    .then(response => response.text())
-    .then(result => {
-        console.log(result)
-        console.log(doc[0])
-    })
+        if(imageUrl){
+            const imgElement = document.createElement('img');
+            imgElement.src = imageUrl;
 
-    .catch(error => console.log('error', error));
+            const previewDiv = document.querySelector('.preview-container')
+            previewDiv.innerHTML = ''
+            previewDiv.appendChild(imgElement);
+        }else{
+            const previewDiv = document.querySelector('.preview-container')
+            previewDiv.innerHTML = 'No img found'
+        }
+      })
+      .catch(error => console.log('error:', error));
+  }
+
+  function hidePreview() {
+    const previewDiv = document.querySelector('.preview-container')
+    previewDiv.innerHTML = '';
+  }
